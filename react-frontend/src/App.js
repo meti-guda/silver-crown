7830bad6c68a3b1a8cc08afcd3564399bc8c47dc
meti-guda <metigmerd@gmail.com>
2026-01-18 17:33:37 +0300

    feat(App): implement routing with React Router for Homepage

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,11 +1,16 @@
-/* eslint-disable react/jsx-no-undef */
-// src/App.js
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Homepage from "./components/pages/Homepage";
 import "./styles/style.css";
 
 function App() {
-  return <Homepage />;
+  return (
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/menu" element={<Homepage />} /> {/* Update later */}
+      <Route path="/contact" element={<Homepage />} /> {/* Update later */}
+    </Routes>
+  );
 }
 
 export default App;
