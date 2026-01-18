86eb48d777737e0fcaf9fbd5eb260fb9c0f2ad63
meti-guda <metigmerd@gmail.com>
2026-01-18 19:36:16 +0300

    feat(pages): update routing to include Contact page

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./components/pages/Homepage";
 import "./styles/style.css";
+import Contact from "./components/pages/Contact";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
-      <Route path="/menu" element={<Homepage />} /> {/* Update later */}
-      <Route path="/contact" element={<Homepage />} /> {/* Update later */}
+      <Route path="/menu" element={<Homepage />} />
+      <Route path="/contact" element={<Contact />} />
     </Routes>
   );
 }
