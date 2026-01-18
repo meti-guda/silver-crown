aae107fdd3e754012f2d74fe562c3e160356d645
meti-guda <metigmerd@gmail.com>
2026-01-18 17:33:50 +0300

    feat(index): wrap App component with BrowserRouter for routing

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // Add this
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      {" "}
+      {/* Wrap App with Router */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
