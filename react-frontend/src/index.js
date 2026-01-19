51db1b1b3e5299b49277392cd5a79dfdca6f7e30
meti-guda <metigmerd@gmail.com>
2026-01-19 20:01:55 +0300

    feat(index): wrap App with MenuProvider in index

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // Add this
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { MenuProvider } from "./components/menu/MenuContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      {" "}
-      {/* Wrap App with Router */}
-      <App />
+      <MenuProvider>
+        <App />
+      </MenuProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
